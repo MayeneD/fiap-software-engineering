10e7378ee6eb4132e4b30f8f98bda4e4c6e1b5a3
Mayene Doria <mayene.contato@gmail.com>
2026-05-20 09:23:57 -0300

    feat: adiciona validação e processamento de transferência

--- aula-07/sistema_transferencia.py
+++ aula-07/sistema_transferencia.py
@@ -21,4 +21,14 @@ class BancoDeDados:
 class ServidorNubank:
     def __init__(self):
         self.banco = BancoDeDados()
-        
+
+    def processar_transferencia(self, user_id: str, valor: float) -> dict:
+    saldo = self.banco.verificar_saldo(user_id)
+
+    if saldo >= valor:
+        self.banco.debitar(user_id, valor)
+        saldo_restante = self.banco.verificar_saldo(user_id)
+        return {"status": "aprovado", "saldo_restante": saldo_restante}
+    else:
+        return {"status": "recusado", "motivo": "saldo insuficiente"}
+
